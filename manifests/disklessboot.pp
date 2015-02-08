c2dc200c8da7a74dc8ee458871f66eab29bf3740
Henk <henk@example.org>
2015-02-08 21:23:31 +0100

    Now netboot works and facter can be run.

--- manifests/disklessboot.pp
+++ manifests/disklessboot.pp
@@ -11,6 +11,8 @@ $szOsName = 'fedora20_x86_64'
 $szInitRamFS = 'initramfs-3.16.7-200.fc20.x86_64.img'
 $szVmLinuz   = 'vmlinuz-3.16.7-200.fc20.x86_64'
 
+package { 'dracut-network': ensure => present, }
+
 file { '/exported': ensure => directory }
 file { "$szDisklessImagesBasePath": ensure => directory, require => File [ '/exported' ] }
 file { "$szDisklessImagesBasePath/$szOsName": ensure => directory, require => File [ "$szDisklessImagesBasePath" ], }
@@ -23,14 +25,22 @@ file { "$szDisklessImagesBasePath/$szOsName": ensure => directory, require => Fi
 #real	4m40.583s
 #rm /exported/disklessimages/fedora20_x86_64/etc/yum.repos.d/*.repo
 # cp /etc/yum.repos.d/local.repo /exported/disklessimages/fedora20_x86_64/etc/yum.repos.d
-# time yum install --installroot=/exported/disklessimages/fedora20_x86_64  --releasever=20  yum kernel grub mdadm 
+# time yum install --installroot=/exported/disklessimages/fedora20_x86_64  --releasever=20  yum kernel grub mdadm net-tools pciutils usbutils facter
+
+# net-tools - ifconfig
+# pciutils - lspci
 
 
-$szDefaultNfsOptionList =  'ro,no_root_squash'
+# strace -f -o /tmp/t usermod --root /exported/disklessimages/fedora20_x86_64 --password \$1\$pee3MHkJ\$0Ggz48t1xbPq/UnZ5SqxX1 root
+
+
+$szDefaultNfsOptionList =  'rw,no_root_squash,async,insecure,no_subtree_check'
+# from: http://xmodulo.com/diskless-boot-linux-machine.html
+
 $szDefaultNfsClientList = hiera ( 'DefaultNfsClientList', '172.16.1.0/255.255.255.0' )
 
 $hNfsExports = {
- "$szDisklessImagesBasePath/fedora20_x86_64" => {
+ "$szDisklessImagesBasePath/$szOsName" => {
              'NfsOptionList' => "$szDefaultNfsOptionList",
              'NfsClientList' => "$szDefaultNfsClientList",
                                         },
@@ -47,6 +57,11 @@ file { "$szTftpBasePath/$szOsName":
   owner  => nobody,
 }
 
+# dracut -f  -m "nfs network base" /var/tftp/fedora20_x86_64/initrd.img
+# dracut -f  --add network /var/tftp/fedora20_x86_64/initrd.img 
+#  chown nobody /var/tftp/fedora20_x86_64/initrd.img
+# dracut -f --debug  root=dhcp  /var/tftp/fedora20_x86_64/initrd.img
+# scp cadm@10.1.233.3:/home/ks/repo/linux/releases/20/Fedora/x86_64/os/images/pxeboot/initrd.img /var/tftp/fedora20_x86_64/
 file { "$szTftpBasePath/$szOsName/initrd.img":
   ensure => present,
   source => "$szDisklessImagesBasePath/$szOsName/boot/$szInitRamFS",
@@ -65,4 +80,10 @@ file { "$szTftpBasePath/pxelinux.cfg/default":
   ensure  => file,
   backup  => false,
   content => template('server-of-diskless-boot/default.erb'),
+}
+
+file { "$szDisklessImagesBasePath/$szOsName/etc/fstab":
+  ensure  => file,
+  backup  => false,
+  content => template('server-of-diskless-boot/etc_fstab.erb'),
 }
